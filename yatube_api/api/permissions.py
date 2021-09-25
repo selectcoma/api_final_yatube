d3e934a76210322593fe67ee5216a1ee1d6d5c07
selectcoma <selectkoma@gmail.com>
2021-09-25 08:18:28 +0200

    first

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+
+class AuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+            )
+
+
+class ReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
